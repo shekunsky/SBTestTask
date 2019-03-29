291fc9c09f6b9580c9c3488d4db7fcf9125733e7
Alex Shekunsky <shekunsky@gmail.com>
2019-03-29 14:30:47 +0200

    Initial Commit

---
+++ SBTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SBTestTask
+//
+//  Created by Alex2 on 3/29/19.
+//  Copyright © 2019 Alex Shekunsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
